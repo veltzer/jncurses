bc6e5757cef65a5fda7841372a9d9b9b92df3c55
Mark Veltzer <mark.veltzer@gmail.com>
2012-01-14 01:40:02 +0200

    fixed up the build on a new ubuntu

---
+++ scripts/ubuntu_install.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+# this scrip will install all the required packages that you need on
+# ubuntu to compile and work with this package.
+
+import subprocess
+
+packs=[
+	'swig', # for creating the interfaces
+	'swig-examples', # for the examples
+	'swig-doc', # for the documentation
+	'gcc-doc', # for development
+]
+
+args=['sudo','apt-get','install']
+args.extend(packs)
+subprocess.check_call(args)
