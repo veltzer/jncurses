80d7609f0944cb4ec32634f91b6803c51f6ce11a
Mark Veltzer <mark.veltzer@gmail.com>
2020-12-13 10:58:16 +0200

    more code

---
+++ config/deps.py
@@ -0,0 +1,12 @@
+d.packs=[
+    # for creating the interfaces
+    'swig',
+    # for the examples
+    'swig-examples',
+    # for the documentation
+    'swig-doc',
+    # for development
+    'gcc-doc',
+    # for swig file
+    'autoconf-archive',
+]
